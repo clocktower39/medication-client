0586d793864715f4f1600539bd82ac4319cc6ca9
clocktower39 <clocktower39@aol.com>
2022-03-17 23:50:16 -0700

    agent profile tables for notes and services

--- src/Components/Profile/AgentProfile.js
+++ src/Components/Profile/AgentProfile.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { Avatar, Container, Grid, Paper, Typography } from "@mui/material";
-import { getAgentInfo, getAgentNotes } from '../../Redux/actions';
-import Loading from '../Loading';
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, Link } from "react-router-dom";
+import { Avatar, Container, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { getAgentInfo, getAgentNotes } from "../../Redux/actions";
+import Loading from "../Loading";
 
 const classes = {
   Avatar: {
@@ -16,35 +16,50 @@ const classes = {
 export default function AgentProfile() {
   const dispatch = useDispatch();
   const params = useParams();
-  const agentProfile = useSelector(state => state.agentProfile);
+  const agentProfile = useSelector((state) => state.agentProfile);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     dispatch(getAgentInfo(params.agentId));
     dispatch(getAgentNotes(params.agentId));
-  }, [dispatch, params.agentId])
+  }, [dispatch, params.agentId]);
 
   useEffect(() => {
     if (agentProfile.agent && agentProfile.notes) {
       setLoading(false);
     }
-  }, [agentProfile])
+  }, [agentProfile]);
 
-  return loading ? <Loading /> : (
+  return loading ? (
+    <Loading />
+  ) : (
     <Container maxWidth="lg" sx={{ padding: "7.5px 0px" }}>
       <Grid container>
         <Grid container component={Paper} sx={classes.PaperSection}>
           <Grid container item xs={4} sx={{ justifyContent: "center" }}>
             <Avatar sx={classes.Avatar} />
           </Grid>
-          <Grid container item xs={8} sx={{ flexDirection: 'column', justifyContent: "center", alignItems: "center" }}>
+          <Grid
+            container
+            item
+            xs={8}
+            sx={{ flexDirection: "column", justifyContent: "center", alignItems: "center" }}
+          >
             <Typography variant="h6">{agentProfile.agent.username}</Typography>
             <Typography variant="h6">{agentProfile.agent.role}</Typography>
-            <Typography variant="h5">{agentProfile.agent.firstName} {agentProfile.agent.lastName}</Typography>
+            <Typography variant="h5">
+              {agentProfile.agent.firstName} {agentProfile.agent.lastName}
+            </Typography>
           </Grid>
-          <Grid container item xs={10} sx={{ justifyContent: 'center', }}>
-            <Grid container item><Typography variant="body1">Supervisor: {agentProfile.agent.supervisor}</Typography></Grid>
-            <Grid container item><Typography variant="body1">Projects: {agentProfile.agent.projects.join(', ')}</Typography></Grid>
+          <Grid container item xs={10} sx={{ justifyContent: "center" }}>
+            <Grid container item>
+              <Typography variant="body1">Supervisor: {agentProfile.agent.supervisor}</Typography>
+            </Grid>
+            <Grid container item>
+              <Typography variant="body1">
+                Projects: {agentProfile.agent.projects.join(", ")}
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
 
@@ -52,18 +67,75 @@ export default function AgentProfile() {
           <Grid container item sx={{ justifyContent: "center" }}>
             <Typography variant="h5">Services</Typography>
           </Grid>
-          <Grid container item>
-            <Typography variant="h5">Table</Typography>
-          </Grid>
+          <TableContainer component={Paper}>
+            <Table size="small">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Date</TableCell>
+                  <TableCell>Account</TableCell>
+                  <TableCell>Service</TableCell>
+                  <TableCell>Summary</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {/* temporary, until services are integrated */}
+                {false ? (
+                  agentProfile.services.map((service) => (
+                    <TableRow key={service._id}>
+                      <TableCell>{service.date}</TableCell>
+                      <TableCell>
+                        <Typography component={Link} to={`/patientProfile/${service.accountId}`}>
+                          {service.accountId}
+                        </Typography>
+                      </TableCell>
+                      <TableCell>{service.service}</TableCell>
+                      <TableCell>{service.summary}</TableCell>
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell>No services </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Grid>
 
         <Grid container component={Paper} sx={classes.PaperSection}>
           <Grid container item sx={{ justifyContent: "center" }}>
             <Typography variant="h5">Notes</Typography>
           </Grid>
-          <Grid container item>
-            {agentProfile.notes.length > 0 ? agentProfile.notes.map(note => <Grid key={`note-${note.date}-${note._id}`} container item xs={12}>{note.note}</Grid>):<>No notes</>}
-          </Grid>
+          <TableContainer component={Paper}>
+            <Table size="small">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Date</TableCell>
+                  <TableCell>Account</TableCell>
+                  <TableCell>Note Summary</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {agentProfile.notes.length > 0 ? (
+                  agentProfile.notes.map((n) => (
+                    <TableRow key={n._id}>
+                      <TableCell>{n.date}</TableCell>
+                      <TableCell>
+                        <Typography component={Link} to={`/patientProfile/${n.accountId}`}>
+                          {n.accountId}
+                        </Typography>
+                      </TableCell>
+                      <TableCell>{n.note}</TableCell>
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell>No notes </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Grid>
       </Grid>
     </Container>
