928bfddfdfe24e7357bf70758d2c9980a307f7a1
clocktower39 <clocktower39@aol.com>
2022-06-11 23:35:14 -0700

    404 not found route/page

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import EnrollContainer from "./Components/Enroll/EnrollContainer";
 import PatientProfile from "./Components/Profile/PatientProfile";
 import PrescriberProfile from "./Components/Profile/PrescriberProfile";
 import AgentProfile from "./Components/Profile/AgentProfile";
-import Footer from './Components/Footer';
-import Login from './Components/Login';
-import Signup from './Components/Signup';
-import AuthRoute from './Components/AuthRoute';
+import Footer from "./Components/Footer";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import NotFoundPage from "./Components/NotFoundPage";
+import AuthRoute from "./Components/AuthRoute";
 import theme from "./theme";
 import "./App.css";
 
@@ -31,31 +32,32 @@ function App() {
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/signup" element={<Signup />} />
 
-          {/* Must be logged in and have JWT token to authenticate */}
-          <Route exact path="/" element={<AuthRoute />}>
-            <Route exact path="/" element={<Home />} />
-          </Route>
-          
-          <Route exact path="/search" element={<AuthRoute />}>
-            <Route exact path="/search" element={<MainSearch />} />
-          </Route>
-          
-          <Route exact path="/enroll" element={<AuthRoute />}>
-            <Route exact path="/enroll" element={<EnrollContainer />} />
-          </Route>
-          
-          <Route exact path="/patientProfile/" element={<AuthRoute />}>
-            <Route exact path="/patientProfile/*" element={<PatientProfile/>} />
-          </Route>
+            {/* Must be logged in and have JWT token to authenticate */}
+            <Route exact path="/" element={<AuthRoute />}>
+              <Route exact path="/" element={<Home />} />
+            </Route>
 
-          <Route exact path="/prescriberProfile/" element={<AuthRoute />}>
-            <Route exact path="/prescriberProfile/*" element={<PrescriberProfile />} />
-          </Route>
+            <Route exact path="/search" element={<AuthRoute />}>
+              <Route exact path="/search" element={<MainSearch />} />
+            </Route>
 
-          <Route exact path="/agentProfile/" element={<AuthRoute />}>
-            <Route exact path="/agentProfile/:agentId" element={<AgentProfile />} />
-          </Route>
+            <Route exact path="/enroll" element={<AuthRoute />}>
+              <Route exact path="/enroll" element={<EnrollContainer />} />
+            </Route>
 
+            <Route exact path="/patientProfile/" element={<AuthRoute />}>
+              <Route exact path="/patientProfile/*" element={<PatientProfile />} />
+            </Route>
+
+            <Route exact path="/prescriberProfile/" element={<AuthRoute />}>
+              <Route exact path="/prescriberProfile/*" element={<PrescriberProfile />} />
+            </Route>
+
+            <Route exact path="/agentProfile/" element={<AuthRoute />}>
+              <Route exact path="/agentProfile/:agentId" element={<AgentProfile />} />
+            </Route>
+
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
           <Footer />
         </Router>
---
+++ src/Components/NotFoundPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+export default function NotFoundPage() {
+  return (
+    <Typography textAlign="center" variant="h4">404: Page Not Found</Typography>
+  )
+}
